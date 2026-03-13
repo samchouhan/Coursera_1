3267612ce6306dd474e86848ade72e67cee9ad84
SAKSHAM_CHOUHAN <131700345+samchouhan@users.noreply.github.com>
2026-03-13 09:37:00 +0530

    Add files via upload

---
+++ Coursera_7.py
@@ -0,0 +1,6 @@
+bill =47.28 # Assign "bill" variable with bill amount
+tip = bill * 15/100 # Multiply by stated tip rate 
+total = bill + tip # Sum the "total" of the "bill" and "tip"
+share = total/4 # Divide "total" by number of friends dining
+print("Each person needs to pay:" + str(share)) # Enter the required string and "share" 
+# Hint: Remember to convert incompatible data 
