8171dd1a9515cad70be1492c8066071a5c592d19
SAKSHAM_CHOUHAN <131700345+samchouhan@users.noreply.github.com>
2026-03-18 21:57:29 +0530

    Update Coursera_9.py

--- Coursera_9.py
+++ Coursera_9.py
@@ -1,7 +1,9 @@
+#This is the basic code for a distance converter from km to m ...
 def convert_distance(km):
     m= km * 1000
     return m
 
-my_trip_kilometers = 55
+my_trip_kilometers = 34
 my_trip_meters = convert_distance(my_trip_kilometers)
 print("The distance in meters is: " + str(my_trip_meters))
+
