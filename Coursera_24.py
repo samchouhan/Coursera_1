d309475859398fd0280eea93fe5008cbbb067d59
SAKSHAM_CHOUHAN <131700345+samchouhan@users.noreply.github.com>
2026-03-19 22:46:15 +0530

    Add files via upload

---
+++ Coursera_24.py
@@ -0,0 +1,4 @@
+for i in range (10, 31,):
+    print(i)
+    
+#Note that the numbers printed were from 10 to 30, and not from 10 to 31. This is because the range function generates numbers up to, but not including, the stop value (in this case, 31).
